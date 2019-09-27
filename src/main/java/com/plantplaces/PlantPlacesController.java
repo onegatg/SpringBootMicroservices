95c0d6f8347edfa9bcded098cfe80f35029ee2f6
Administrator <Administrator@DESKTOP-099UQRG>
2019-09-27 15:16:24 -0400

    Work done in class, Added index page, and added image to start page

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -1,6 +1,8 @@
 package com.plantplaces;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,8 +13,28 @@ public class PlantPlacesController {
 	 * Handle the /start endpoint
 	 * @return 
 	 */
-	@RequestMapping("/start")
-	public String start() {
+	@GetMapping("/start")	
+	public String read() {
+		return "start";
+	}
+	
+	@GetMapping(value="/start", params= {"loyalty=blue"})	
+	public String readBlue() {
+		return "start";
+	}
+	
+	@GetMapping(value="/start", params= {"loyalty=silver"})	
+	public String readSilver() {
+		return "start";
+	}
+	
+	@GetMapping(value="/start", headers= {"content-type=text/json"})	
+	public String readJSON() {
+		return "start";
+	}
+	
+	@PostMapping("/start")
+	public String create() {
 		return "start";
 	}
 	
